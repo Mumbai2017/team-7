4c3b787f5fc4eed88bbf6af398708b0666a2a736
Rudresh Panchal <rudreshpanchal11@gmail.com>
2017-07-29 16:52:04 +0530

    added sakhi model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Sakhi
+admin.site.register(Sakhi)
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
+class Sakhi(models.Model):
+	user = models.OneToOneField(User)
+	lat = models.CharField(max_length=100,blank=True)
+	lng = models.CharField(max_length=100,blank=True)
+	nachni = models.IntegerField(default=0,blank=True)
+	mari = models.IntegerField(default=0,blank=True)
+	oat = models.IntegerField(default=0,blank=True)
+
