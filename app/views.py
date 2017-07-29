2fa3e90a65824cf24c8d3ab78ebad94ce0392392
Rudresh Panchal <rudreshpanchal11@gmail.com>
2017-07-29 15:54:30 +0530

    initialized project and app

---
+++ app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from twilio.rest import TwilioRestClient
+# Create your views here.
+
+def hello_world(request):
+	user_all = User.objects.all()
+	if request.method == 'GET':
+		print 'hello'
+		sms_body = request.GET.get('Body')
+		print sms_body
+	#return HttpResponse('HI!')
+
+def sms(request):
+	if request.method == 'GET':
+		print 'hello'
+		sms_body  = request.GET.get('Body')
+		print sms_body
+
+def register_sakhi(request):
+	if request.method == 'POST':
+		name =  request.POST.get('name')
+		passsword = request.POST.get('passsword')
+		return HttpResponse(name)
+	else:
+		return render(request,'sakhi_register.html')
+
+'''
+def get_sms(request):
+	ACCOUNT_SID = "AC2deb88c500af87f3abf68e0977e3dd8d" 
+	AUTH_TOKEN = "3d508103252df724e85bb633c0455851" 
+ 	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+ 	messages = client.sms.messages.list()
+ 	print len(messages)
+ 	for message in messages:
+ 		if message.direction == 'inbound':
+ 			print message.body
+ 			print message.sid
+ 	return HttpResponse('HI!')
+'''
